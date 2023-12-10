49d65966c43c4cba5d01241a9b9ceb241e618eb5
AlfaDent <andreyevsvyatoslav@gmail.com>
2023-12-10 19:44:03 +0700

    add g/s to empDTO and add constr to emp

--- src/main/java/com/example/taskmanagementsystem/DTO/EmployeeDTO.java
+++ src/main/java/com/example/taskmanagementsystem/DTO/EmployeeDTO.java
@@ -1,8 +1,11 @@
 package com.example.taskmanagementsystem.DTO;
 
 import com.example.taskmanagementsystem.models.Employee;
+import lombok.Getter;
+import lombok.Setter;
 
-
+@Getter
+@Setter
 public class EmployeeDTO extends Employee {
     private String name;
     private String lastname;
