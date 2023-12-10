c77239a84fde7117686508f38f33c4560577c222
AlfaDent <andreyevsvyatoslav@gmail.com>
2023-12-10 19:17:14 +0700

    Fill EmployeeController

--- src/main/java/com/example/taskmanagementsystem/controllers/EmployeeController.java
+++ src/main/java/com/example/taskmanagementsystem/controllers/EmployeeController.java
@@ -1,11 +1,40 @@
 package com.example.taskmanagementsystem.controllers;
 
+import com.example.taskmanagementsystem.DTO.EmployeeDTO;
+import com.example.taskmanagementsystem.models.Employee;
 import com.example.taskmanagementsystem.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
+
+    @GetMapping(value = "/employees")
+    public List<Employee> getAllEmployees(){
+        return employeeService.getAllEmployees();
+    }
+    @GetMapping(value = "/employee/{id}")
+    public Optional<Employee> getEmployeeById(@PathVariable Long id){
+        return employeeService.getEmployeeById(id);
+    }
+    @PostMapping(value = "/employee")
+    public ResponseEntity<Employee> createEmployee(@RequestBody EmployeeDTO employeeDTO){
+        Employee employee = employeeService.createEmployee(employeeDTO);
+        return new ResponseEntity<>(employee, HttpStatus.CREATED);
+    }
+    @DeleteMapping(value = "/employee/{id}")
+    public void deleteEmployee(@PathVariable Long id){
+        employeeService.deleteEmployeeById(id);
+    }
+    @PutMapping(value = "/employee/{id}")
+    public void editEmployee(@PathVariable Long id, @RequestBody EmployeeDTO employeeDTO){
+        employeeService.editEmployeeById(id, employeeDTO);
+    }
 }
